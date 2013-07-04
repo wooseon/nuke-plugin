b94434dd8eb7311f8c8a4f5f5c7fe59636ed60dc
Richard Greenwood <richardsgreenwood@gmail.com>
2013-07-04 14:32:41 -0700

    generic setup

--- init.py
+++ init.py
@@ -10,7 +10,6 @@ nuke.pluginAddPath('./lut')
 
 import os, nukescripts, platform
 import pipeline
-import sendToAvconv
 
 # SET KNOB DEFAULTS
 # ////////////////////////////////////////////////////////////////////////////////
@@ -18,12 +17,10 @@ import sendToAvconv
 # WRITE NODE
 # use this instead of nuke.addBeforeRender so that artists can remove it locally if needed
 nuke.knobDefault('Write.beforeRender', 'pipeline.createWriteDir()')
-nuke.knobDefault('Write.afterRender', 'sendToAvconv.sendToAvconv()')
 
 # ROOT
 nuke.knobDefault('Root.project_directory', '[python {nuke.script_directory()}]/../')
-nuke.addFormat( '2048 1152 0 0 2048 1152 1 2k_16:9' )
-nuke.knobDefault('Root.format', '2k_16:9')
+nuke.knobDefault('Root.format', 'HD')
 nuke.knobDefault('Root.proxy_type', 'scale')
 nuke.knobDefault('Root.proxy_scale', '.5')
 nuke.knobDefault('Root.fps', '23.976')
@@ -36,12 +33,12 @@ import reformat_presets
 reformat_presets.nodePresetReformat()
 
 # LUTs
-#CB
-nuke.knobDefault('Viewer.viewerProcess', 'None')
-nuke.knobDefault('monitorLut', 'linear')
-nuke.knobDefault('logLut', 'linear')
-nuke.knobDefault('int8Lut', 'linear')
-nuke.knobDefault('int16Lut', 'linear')
+#TF
+#nuke.knobDefault('Viewer.viewerProcess', '')
+#nuke.knobDefault('monitorLut', '')
+#nuke.knobDefault('logLut', '')
+#nuke.knobDefault('int8Lut', '')
+#nuke.knobDefault('int16Lut', '')
 
 # OTHER
 # ////////////////////////////////////////////////////////////////////////////////
